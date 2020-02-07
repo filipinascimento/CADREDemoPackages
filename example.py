fb13aecea481c689d58907535471324c9e974c20
Filipi Nascimento Silva <filipinascimento@gmail.com>
2020-02-07 11:01:46 -0500

    initial commit

---
+++ example.py
@@ -0,0 +1,49 @@
+import os.path
+import query2xnet
+import xnet2wordcloud
+import xnet2figure
+import xnet2communities_wordcloud
+import importlib
+
+importlib.reload(query2xnet);
+importlib.reload(xnet2wordcloud);
+importlib.reload(xnet2figure);
+importlib.reload(xnet2communities_wordcloud);
+
+minYear = 2000
+minKCore = 1
+queryID = "ee732263-ef2b-4912-87c1-de0bd1b54a11";
+workingDirectory = "DemoData"
+queriesDirectory = os.path.join(workingDirectory,"query-results");
+outputNetworkDirectory = os.path.join(workingDirectory,"networks");
+outputFiguresDirectory = os.path.join(workingDirectory,"figures");
+
+
+
+os.makedirs(outputNetworkDirectory, exist_ok=True)
+os.makedirs(outputFiguresDirectory, exist_ok=True)
+
+query2xnet.mag_query_input_to_xnet(
+	os.path.join(queriesDirectory,"%s.csv"%queryID),
+	os.path.join(queriesDirectory,"%s_edges.csv"%queryID),
+	os.path.join(outputNetworkDirectory,"%s.xnet"%queryID)
+)
+
+xnet2wordcloud.xnet_input_to_wordcloud(
+	os.path.join(outputNetworkDirectory,"%s.xnet"%queryID),
+	os.path.join(outputFiguresDirectory,"%s_wordcloud.pdf"%queryID),
+)
+
+xnet2figure.xnet_input_to_figure(
+	os.path.join(outputNetworkDirectory,"%s.xnet"%queryID),
+	os.path.join(outputFiguresDirectory,"%s_vis.pdf"%queryID),
+	minYear=minYear,
+	minKCore=minKCore
+)
+
+xnet2communities_wordcloud.xnet_input_to_communities_wordcloud(
+	os.path.join(outputNetworkDirectory,"%s.xnet"%queryID),
+	os.path.join(outputFiguresDirectory,"%s_communities_wc.pdf"%queryID),
+	minYear=minYear,
+	minKCore=minKCore
+)
---
+++ xnet2wordcloud/__init__.py
@@ -0,0 +1,59 @@
+
+import sys
+import os
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import igraph as ig
+from wordcloud import WordCloud
+
+try:
+	from . import xnet as xn
+except ImportError as error:
+	import xnet as xn
+
+
+#Extract major component and obtain community structure using infomap
+
+def xnet_to_wordcloud(argv):
+	input_file_string = argv[3]
+	output_files_string = argv[4]
+	output_location = argv[5]
+	input_files = []
+	output_files = []
+	if ',' in input_file_string:
+		input_file_string = "" + input_file_string + ""
+		input_files = input_file_string.split(",")
+	else:
+		input_files.append(input_file_string)
+	if ',' in output_files_string:
+		output_files_string = "" + output_files_string + ""
+		output_files = output_files_string.split(',')
+	else:
+		output_files.append(output_files_string)
+	
+	output_file = output_location + '/' + output_files[0]
+	xnet_input_to_wordcloud(input_files[0], output_file)
+
+def xnet_input_to_wordcloud(input_file, output_file):
+	graph = xn.xnet2igraph(input_file)
+	wc = WordCloud(background_color="white", max_words=2000, scale=10,
+		contour_width=3, contour_color='white')
+	if("paper_abstract" not in graph.vertex_attributes()):
+		textData = graph.vs["original_title"]
+	else:
+		textData =  ["%s. %s"%(titleAndAbstract) for titleAndAbstract in zip(graph.vs["original_title"],graph.vs["paper_abstract"])]
+	wc.generate("\n".join(textData))
+	wc.to_file(output_file)
+
+def xnet_query_id_to_wordcloud(queryID):
+	os.makedirs("figures", exist_ok=True)
+	xnet_input_to_wordcloud(
+		"networks/%s.xnet"%queryID,
+		"figures/%s_wordcloud.pdf"%queryID
+	)
+
+if __name__ == "__main__":
+	argv = sys.argv
+	xnet_to_wordcloud(argv)
